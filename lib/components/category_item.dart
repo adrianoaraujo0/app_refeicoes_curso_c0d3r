624045849247653647981f3c6e096487ae9f6914
Adriano <adriano@grus.local>
2022-11-04 09:00:23 -0300

    c.184 - Navegando para uma tela nova

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,30 +1,45 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:refeicoes_curso/models/category.dart';
+import '../ui/categories_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final Category category;
 
   const CategoryItem({required this.category ,super.key});
 
+  void _selectCategory(BuildContext context){
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) {
+          return const CategoriesMealsScreen();
+        }, 
+      )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15),
-        gradient: LinearGradient(
-          colors: [
-            category.color.withOpacity(0.5),
-            category.color
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight
-        )
-      ),
-      child: Text(
-        category.title,
-        style: Theme.of(context).textTheme.titleMedium,
+    return InkWell(
+      onTap: () => _selectCategory(context),
+      borderRadius: BorderRadius.circular(15),
+      splashColor: Theme.of(context).primaryColor,
+      child: Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15),
+          gradient: LinearGradient(
+            colors: [
+              category.color.withOpacity(0.5),
+              category.color
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight
+          )
+        ),
+        child: Text(
+          category.title,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
       ),
     );
   }
--- lib/ui/category_screen.dart
+++ lib/ui/category_screen.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:refeicoes_curso/components/category_item.dart';
 import 'package:refeicoes_curso/data/dummy_data.dart';
 
