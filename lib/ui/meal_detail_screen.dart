de868074ea7e5f5f9a473137e4f044369db2335b
adrianoaraujo0 <adriano.amdeiros@hotmail.com>
2022-11-06 15:33:02 -0300

    c.195 - Tela detalhe refeicao 2

--- lib/ui/meal_detail_screen.dart
+++ lib/ui/meal_detail_screen.dart
@@ -1,9 +1,30 @@
 import 'package:flutter/material.dart';
-
 import '../models/meal.dart';
 
 class MealDetailScreen extends StatelessWidget {
   MealDetailScreen({super.key});
+
+   Widget _createSectionTitle(BuildContext context, String title){
+    return Container(
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: Text(title, style: Theme.of(context).textTheme.titleMedium),
+    );
+  }
+
+  Widget _createSectionContainer(Meal meal, Widget widget){
+    return  Container(
+      width: 330,
+      height: 250,
+      padding: const EdgeInsets.all(10),
+      margin: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10)
+      ),
+      child: widget
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,50 +33,58 @@ class MealDetailScreen extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(title: Text(meal.title)),
-      body: Column(
-        children: [
-          Container(
-            height: 300,
-            width: double.infinity,
-            child: Image.network(
-              meal.imageUrl,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10),
-            child: Text(
-              "Ingredientes",
-              style: Theme.of(context).textTheme.titleMedium,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 300,
+              width: double.infinity,
+              child: Image.network(
+                meal.imageUrl,
+                fit: BoxFit.cover,
               ),
-          ),
-          Container(
-            width: 300,
-            height: 250,
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.all(10),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              border: Border.all(color: Colors.grey),
-              borderRadius: BorderRadius.circular(10)
             ),
-            child: ListView.builder(
-              itemCount: meal.ingredients.length,
-              itemBuilder: (context, index) {
-                return Card(
-                  color: Theme.of(context).colorScheme.primary,
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                      vertical: 5,
-                      horizontal: 10
+            _createSectionTitle(context, "Ingredientes"),
+            _createSectionContainer(
+              meal,
+              ListView.builder(
+                itemCount: meal.ingredients.length,
+                itemBuilder: (context, index) {
+                  return Card(
+                    color: Theme.of(context).colorScheme.primary,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 5,
+                        horizontal: 10
+                      ),
+                      child: Text(meal.ingredients[index]),
                     ),
-                    child: Text(meal.ingredients[index]),
-                  ),
+                  );
+                },
+              ),
+            ),
+          _createSectionTitle(context, "Passos"),
+          _createSectionContainer(
+            meal,
+            ListView.builder(
+              itemCount: meal.steps.length,
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    ListTile(
+                      leading: CircleAvatar(
+                        child: Text('${index + 1}'),
+                      ),
+                      title: Text(meal.steps[index]),
+                    ),
+                    const Divider(color: Colors.black)
+                  ],
                 );
               },
-            ),
-          ),
-        ]
+            )
+          )
+          ]
+        ),
       ),
     );
   }
